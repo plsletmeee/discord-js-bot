27f1ab65de6ca0ec544bf7cf389be7280e508842
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-06-04 18:12:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -54,7 +54,7 @@ module.exports = {
     ENABLED: true,
     IDLE_TIME: 60, // Time in seconds before the bot disconnects from an idle voice channel
     MAX_SEARCH_RESULTS: 5,
-    DEFAULT_SOURCE: "YT", // YT = Youtube, YTM = Youtube Music, SC = SoundCloud
+    DEFAULT_SOURCE: "SC", // YT = Youtube, YTM = Youtube Music, SC = SoundCloud
     // Add any number of lavalink nodes here
     // Refer to https://github.com/freyacodes/Lavalink to host your own lavalink server
     LAVALINK_NODES: [
